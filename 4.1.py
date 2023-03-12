354e24227e4f9741af1b40d3cfd6d443b4d0fb08
Олег <mishin415@yandex.ru>
2023-03-12 22:35:44 +0300

    4.1

---
+++ 4.1.py
@@ -0,0 +1,12 @@
+print("Введите два числа и знак +, -, *, /")
+a = int(input())
+b = int(input())
+c = input()
+if c == "+":
+    print(a+b)
+if c == "-":
+    print(a-b)
+if c == "*":
+    print(a*b)
+if c == "/":
+    print(a/b)
