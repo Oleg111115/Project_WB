e096214f860952f82910fed392d1f9981377cb58
Oleg_Mishin <126604810+Oleg111115@users.noreply.github.com>
2023-05-11 09:02:51 +0300

    Delete 4.2.1_Рекурсия.py

--- 4.2.1_Рекурсия.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Размер вклада пользователя: "))
-m = int(input("Срок вклада в годах: "))
-def many(n,m):
-    return 0 if m == 0 else n+many(n, m-1)
-print("Размер вклада, через", m, "лет:", many(n, m))
