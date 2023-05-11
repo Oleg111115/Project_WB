79652a9de6f1d609083eb7dbdcd9300084d9191b
Oleg_Mishin <126604810+Oleg111115@users.noreply.github.com>
2023-05-11 09:03:05 +0300

    Delete 4.2.2_Рекурсия.py

--- 4.2.2_Рекурсия.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Размер вклада пользователя: "))
-m = int(input("Срок вклада в годах: "))
-c = int(input("Процентная ставка: "))
-def many(n,m):
-    if (m==0): return n
-    many_up = n*c/100
-    return many(n+many_up, m-1)
-print("Размер вклада, через", m, "лет:", many(n,m))
