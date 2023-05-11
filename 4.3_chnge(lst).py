46f8985cfe44aefef5cf01edc2016bec5c997ead
Oleg_Mishin <126604810+Oleg111115@users.noreply.github.com>
2023-05-11 09:03:27 +0300

    Delete 4.3_chnge(lst).py

--- 4.3_chnge(lst).py
+++
@@ -1,7 +0,0 @@
-def change(lst):
-    last_element = lst.pop()
-    first_element = lst.pop(0)
-    lst.append(first_element)
-    lst.insert(0, last_element)
-    return lst
-print(change(['т', 'а', 'м', 'о', 'л', 'е', 'с',]))
