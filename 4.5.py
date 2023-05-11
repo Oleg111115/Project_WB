e3f175895a3f7ffa808bcc5a85435ad3e81c0d14
Oleg_Mishin <126604810+Oleg111115@users.noreply.github.com>
2023-05-11 09:03:47 +0300

    Delete 4.5.py

--- 4.5.py
+++
@@ -1,7 +0,0 @@
-list_of_numbers = input('введите список чисел: ').split(' ')
-a = 1
-for i in range(1, len(list_of_numbers)):
-    if list_of_numbers[i - 1] != list_of_numbers[i]:
-        a += 1
-print(a)
-
