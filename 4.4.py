ea2e867da9853d235df45e3dc1b70cbc19817880
Oleg_Mishin <126604810+Oleg111115@users.noreply.github.com>
2023-05-11 09:03:37 +0300

    Delete 4.4.py

--- 4.4.py
+++
@@ -1,6 +0,0 @@
-list_of_numbers = input('введите список чисел: ').split(' ')
-a = 0
-for i in range(1, len(list_of_numbers)-1):
-    if list_of_numbers[i-1] < list_of_numbers[i] > list_of_numbers[i+1]:
-        a += 1
-print(a)
